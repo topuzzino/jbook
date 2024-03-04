793f2b98e26563e859b6e9939be37f84bdfcf4bf
Evgenia Gertsovskaya <topuzzino@hotmail.de>
2024-03-04 15:48:56 +0100

    Forgot to add format to a file

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,32 +1,27 @@
-import * as esbuild from 'esbuild-wasm'
+import * as esbuild from 'esbuild-wasm';
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResole', args)
-        return { path: args.path, namespace: 'a' }
-      })
+        console.log('onResole', args);
+        return { path: args.path, namespace: 'a' };
+      });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args)
+        console.log('onLoad', args);
 
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
+              import message from 'tiny-test-pkg';
               console.log(message);
             `,
-          }
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
-          }
+          };
         }
-      })
+      });
     },
-  }
-}
+  };
+};
