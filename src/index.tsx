236b381539da8a848960fe2bcb6aa8c26a176e51
Evgenia Gertsovskaya <topuzzino@hotmail.de>
2024-04-09 23:21:05 +0200

    Refactor startService in a bundler component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import ReactDOM from 'react-dom/client'
-import * as esbuild from 'esbuild-wasm'
-import { useState, useEffect, useRef, SetStateAction } from 'react'
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+import { useState } from 'react'
 import CodeEditor from './code-editor'
 import Preview from './preview'
+import bundle from './bundler'
 
 const el = document.getElementById('root')
 const root = ReactDOM.createRoot(el!)
@@ -11,36 +10,10 @@ const root = ReactDOM.createRoot(el!)
 const App = () => {
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
-  const ref = useRef<any>()
-
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-    })
-  }
-
-  useEffect(() => {
-    startService()
-  }, [])
 
   const handleClick = async () => {
-    if (!ref.current) {
-      return
-    }
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    })
-
-    setCode(result.outputFiles[0].text)
+    const output = await bundle(input)
+    setCode(output)
   }
 
   return (
