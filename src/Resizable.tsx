a93c4bca1044dc1270fd197efc27f264b9f8b1c3
Evgenia Gertsovskaya <topuzzino@hotmail.de>
2024-04-17 23:55:41 +0200

    Rename Resizable files

---
+++ src/Resizable.tsx
@@ -0,0 +1,15 @@
+import { ResizableBox } from 'react-resizable'
+import './Resizable.css'
+
+interface ResizableProps {
+  direction: 'horizontal' | 'vertical'
+  children?: React.ReactNode
+}
+
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => (
+  <ResizableBox height={300} width={Infinity} resizeHandles={['s']}>
+    {children}
+  </ResizableBox>
+)
+
+export default Resizable
